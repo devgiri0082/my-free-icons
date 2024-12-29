3c3ab52dd14b289c562ed3b81264ba51f8cd8bd8
Dev Giri <devgiri0082@gmail.com>
2024-12-29 22:42:32 +0545

    fix source

--- svg.js
+++ svg.js
@@ -4,23 +4,23 @@ async function main() {
     const svgs = "./svgs"
     try {
         const content = await fs.readdir(svgs)
-        const types = new Set()
-        const chunk= []
+        const chunk = []
         content.forEach(icon => {
-            if(!icon.includes("brand")) {
-                if(chunk.length === 100) {
+            if (!icon.includes("brand")) {
+                if (chunk.length === 100) {
                     final.push([...chunk])
                     chunk.length = 0
                 }
                 chunk.push({
                     title: icon.split(".")[0],
-                    link: `https://raw.githubusercontent.com/free-icons/free-icons/refs/heads/master/svgs/${icon}`
+                    link: `https://raw.githubusercontent.com/devgiri0082/my-free-icons/refs/heads/master/svgs/${icon}`
                 })
             }
         })
-        
+
         fs.writeFile("./svg.json", JSON.stringify(final))
     } catch (err) {
         console.log(err)
     }
+}
 main()
