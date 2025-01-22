50665cea8db11637c92858a3be35a39e96a0b760
Dev Giri <devgiri0082@gmail.com>
2025-01-22 13:32:40 +0545

    add preivew images

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-const fs = require("node:fs/promises");
-
 async function main() {
-    path = "./svg.json"
     try {
-        const data= await fs.readFile(path)
-        const iconArr =  JSON.parse(data.toString())
-        console.log(iconArr[0])
+    const path = "https://raw.githubusercontent.com/devgiri0082/my-free-icons/refs/heads/master/svg.json"
+    const res = await fetch(path)
+    const data = await res.json();
+    console.log(data[0])
     } catch(err) {
         console.log(err)
     }
