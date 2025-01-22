6ea5ca47928ad7b6d54c8054b93b5542a330a602
Dev Giri <devgiri0082@gmail.com>
2025-01-22 15:04:23 +0545

    change the way svg icon is saved

--- svg.js
+++ svg.js
@@ -1,27 +1,29 @@
 const fs = require("node:fs/promises");
 async function main() {
-    const final = [];
     const svgs = "./svgs";
     try {
         const collections = await fs.readdir(svgs);
         const icons_collection = {};
-        collections.forEach(async (collection) => {
-            if (collection.startsWith(".")) return;
+        for (const collection of collections) {
+            if (collection.startsWith(".")) continue;
             const icons = await fs.readdir(`${svgs}/${collection}`);
             const each_col = {
                 name: collection,
                 icons: [],
             };
-            icons.forEach(icon => {
+            icons.forEach((icon) => {
                 if (icon.startsWith(".")) return;
                 each_col.icons.push({
                     title: icon.split(".")[0],
-                    link: `https://raw.githubusercontent.com/devgiri0082/my-free-icons/refs/heads/master/svgs/${icon}`
-                })
-            }
-        });
-
-        fs.writeFile("./svg.json", JSON.stringify(final));
+                    link:
+                        `https://raw.githubusercontent.com/devgiri0082/my-free-icons/refs/heads/master/svgs/${collection}/${icon}`,
+                });
+            });
+            console.log(collection);
+            icons_collection[collection] = each_col;
+        }
+        //console.log(icons_collection);
+        fs.writeFile("./svg.json", JSON.stringify(icons_collection));
     } catch (err) {
         console.log(err);
     }
